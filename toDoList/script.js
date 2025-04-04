2d71fe94059570530da9a608be86afc52cc3a93c
Mamuka Axalkaci <141028464+Mamukie6969@users.noreply.github.com>
2025-04-04 23:23:59 +0400

    Add files via upload

---
+++ toDoList/script.js
@@ -0,0 +1,37 @@
+const button = document.getElementById('Button');
+const BoxForInput = document.getElementById('BoxForInput');
+const List = document.getElementById('List');
+
+const savedItems = JSON.parse(localStorage.getItem('listItems')) || [];
+savedItems.forEach(text => addItemToList(text));
+
+button.addEventListener('click', function () {
+    const text = BoxForInput.value;
+
+    if (text) {
+        addItemToList(text);
+        BoxForInput.value = '';
+    }
+});
+
+function addItemToList(text) {
+    const item = document.createElement('li');
+    item.innerText = text;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+
+    removeButton.addEventListener('click', function () {
+        List.removeChild(item);
+        saveItems();
+    });
+
+    item.appendChild(removeButton);
+    List.appendChild(item);
+    saveItems();
+}
+
+function saveItems() {
+    const items = Array.from(List.children).map(li => li.innerText.replace('Remove', ''));
+    localStorage.setItem('listItems', JSON.stringify(items));
+}
